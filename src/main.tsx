37dcd6c621e89ea354992d23df5314ebcc373419
LeoLafore855 <127517554+LeoLeBgDu85@users.noreply.github.com>
2025-01-28 14:28:04 +0100

    feat: Ajout du composant instruction

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import InstructionLightbox from "./components/Instruction/Instruction";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+const App = () => (
+  <>
+    <InstructionLightbox />
+  </>
+);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
