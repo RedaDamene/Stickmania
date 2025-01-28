14fe127e6a728bee1299b03980c1601c41a5cc11
Potowai <81572547+Potowai@users.noreply.github.com>
2025-01-28 12:06:08 +0100

    init app

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Inclure les fichiers React et TypeScript
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
