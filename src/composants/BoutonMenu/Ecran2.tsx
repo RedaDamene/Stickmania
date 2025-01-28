10a8885ae2a8e4e6a50dfc6abc958abc738dc662
badaboom21 <130473771+badaboom21@users.noreply.github.com>
2025-01-28 17:07:08 +0100

    feat-bouton Pause
    - Ajout d'un bouton disponible qui se met en haut à gauche de l'écran
    - Permet de faire apparaître une pop up de "Pause"
    - Le bouton QUITTER permet de revenir à la racine --> Menu standard

---
+++ src/composants/BoutonMenu/Ecran2.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import BoutonMenu from "./BoutonMenu";
+
+
+function Ecran2() {
+  return (
+    <div>
+      <BoutonMenu />
+      <h2>Bienvenue sur l'Écran 2</h2>
+      <p>Ceci est l'écran 2.</p>
+    </div>
+  );
+}
+
+export default Ecran2;
