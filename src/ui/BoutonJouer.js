31cb1e25aa6b0b587ec71772383281a78417abbf
P3psHy <76128319+P3psHy@users.noreply.github.com>
2025-01-28 14:12:17 +0100

    feat/12 Ajotu du bouton jouer

--- src/ui/BoutonJouer.js
+++ src/ui/BoutonJouer.js
@@ -1,13 +1,19 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 
+const PlayButton = () => {
+  // Gestion des styles et comportement
+  const buttonStyle =
+    "bg-green-500 text-white font-bold py-2 px-4 rounded-2xl hover:bg-green-600 transition-all duration-300 shadow-lg hover:shadow-xl active:scale-95";
 
-
-
-
-return(
-<body>
+  return (
+    // Structure HTML
     <div>
-
+        <button className={buttonStyle}>
+        Jouer
+        </button>
     </div>
-</body>
-)
+
+  );
+};
+
+export default PlayButton;
