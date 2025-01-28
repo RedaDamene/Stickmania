325a1c392df831e9161cf3199a0780d4ffeef54b
Enzo <thezozo85@gmail.com>
2025-01-28 14:02:33 +0100

    feat: ajout fonctionnalité de score (pour le moment increment quand on clique)

---
+++ src/composants/Score/Score.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+function Score() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <label onClick={() => setCount((count) => count + 1)}>
+            Score {count}
+        </label>
+      </div>
+    </>
+  )
+}
+
+export default Score
