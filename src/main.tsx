f0676b3a15b0d08bf704e6ade500d2f62e6fbc36
LeoLafore855 <127517554+LeoLeBgDu85@users.noreply.github.com>
2025-01-28 15:08:03 +0100

    feat: ajustement du pull request vu avec alexis

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import InstructionLightbox from "./components/Instruction/Instruction";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-const App = () => (
-  <>
-    <InstructionLightbox />
-  </>
-);
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </StrictMode>,
+)
