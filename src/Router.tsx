4b9415651a3bb45b0220eed19f4ea9f3429d8e39
Potowai <81572547+Potowai@users.noreply.github.com>
2025-01-29 09:59:37 +0100

    Update Router.tsx

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import App from "./App";
-import JeuDuClavier from "./pages/JeuDuClavier/JeuDuClavier";
 import JeuTiming from "./pages/Jeu-timing/Jeu-timing";
+
 export const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="/app" element="Hello World" />
-                <Route path="/app/jeuDuClavier" element={<JeuDuClavier />} />
                 <Route path="/app/jeuTiming" element={<JeuTiming />} />
+                <Route path="*" element="404" />
             </Routes>
         </BrowserRouter>
     );
