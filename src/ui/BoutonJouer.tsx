4685b689eaeda05ccc50c7a4b907b4270be36920
Potowai <81572547+Potowai@users.noreply.github.com>
2025-01-29 12:01:20 +0100

    Revert "fix: gros dev"
    This reverts commit ecdc36e68cc4a657707eec2da3ca5c584fd6c917.

--- src/ui/BoutonJouer.tsx
+++ src/ui/BoutonJouer.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function BoutonJouer () {
   return (
     <>
-      <Link to="/app">
+      <Link to="/app/jeuTiming">
         <button className="">
           <img src="/images/BoutonJouer.png" alt="Jouer" />
         </button>
