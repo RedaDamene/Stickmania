ba2ed8bba3d598210a58083ca2469c8e2840596e
LeoLafore855 <127517554+LeoLeBgDu85@users.noreply.github.com>
2025-01-29 10:30:56 +0100

    feat: Ajout du jeu du morpion vs BOT avec le routing

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import App from "./App";
 import JeuTiming from "./pages/Jeu-timing/Jeu-timing";
+import JeuMorpion from "./pages/Morpion/Jeu-morpion";
 
 export const Router = () => {
     return (
@@ -9,6 +10,7 @@ export const Router = () => {
                 <Route path="/" element={<App />} />
                 <Route path="/app" element="Hello World" />
                 <Route path="/app/jeuTiming" element={<JeuTiming />} />
+                <Route path="/app/jeuMorpion" element={<JeuMorpion />} />
                 <Route path="*" element="404" />
             </Routes>
         </BrowserRouter>
